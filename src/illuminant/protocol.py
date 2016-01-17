7c467f214f6f81c87fb00d1f18d4da23ce1d3931
Oopsno <yufeiliu94@gmail.com>
2016-01-17 15:47:35 +0800

    added `DaemonProtocol` init & default impl

--- src/illuminant/protocol.py
+++ src/illuminant/protocol.py
@@ -2,12 +2,19 @@
 Illuminant XML-RPC based protocol
 """
 
+from xmlrpclib import ServerProxy
+
 
 def interrupted_response(value):
     return 1, 'Interrupted by daemon', value
 
 
 class DaemonProtocol(object):
+    def __init__(self, daemon_uri, illuminant_uri):
+        self.daemon_uri = daemon_uri
+        self.illuminant_uri = illuminant_uri
+        self.illuminant_proxy = ServerProxy(illuminant_uri)
+
     # METHODS FOR ILLUMINANT TO CONTROL SELL
     def launch(self, package_name):
         pass
@@ -29,13 +36,13 @@ class DaemonProtocol(object):
         """
         This will register service to illuminant, not Master
         """
-        pass
+        self.illuminant_proxy.regCell(service, service_api, self.daemon_uri)
 
     def unregisterService(self, caller_id, service, service_api):
         """
         This will unregister service from illuminant, not Master
         """
-        pass
+        self.illuminant_proxy.unregCell(service, service_api, self.daemon_uri)
 
     # THESE METHODS INTERRUPT ORIGINAL CALLS AND DO NOTHING
     def registerPublisher(self, caller_id, topic, topic_type, caller_api):
