fd28e08de4dbf8c44743e1baf3ffdba59c206638
Oopsno <yufeiliu94@gmail.com>
2016-01-17 19:50:29 +0800

    added test_LRU

--- test/test_Daemon.py
+++ test/test_Daemon.py
@@ -6,10 +6,15 @@ from illuminant.illuminant import Illuminant
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from threading import Thread
 import time
+import random
 
 
 def port_to_uri(port):
     return 'http://localhost:{}'.format(port)
+
+
+def uri_to_port(uri):
+    return int(uri[17:])
 
 
 def run_illuminant(port):
@@ -45,8 +50,8 @@ CALLER_API = 'http://localhost:65535'
 
 class Daemon(TestCase):
     def setUp(self):
-        illuminant_port = 33333
-        daemon_port = 22322
+        illuminant_port = random.randrange(10000, 30000)
+        daemon_port = random.randrange(10000, 30000)
         self.illuminant_obj = run_illuminant(illuminant_port)
         self.daemon_handler = run_daemon(daemon_port, port_to_uri(illuminant_port))
         self.d = ServerProxy(port_to_uri(daemon_port))
@@ -61,4 +66,14 @@ class Daemon(TestCase):
         records = self.illuminant_handler.records
         self.assertEqual(len(records[SERVICE]), 1)
         res = self.d.unregisterService(CALLER_ID, SERVICE, port_to_uri(0))
-        # self.assertEqual(res[0], 1)
+        self.assertEqual(res[0], 1)
+
+    def test_LRU(self, times=10):
+        for i in range(times):
+            self.d.registerService(CALLER_ID, SERVICE, port_to_uri(i), CALLER_API)
+        ports = []
+        for i in range(times):
+            res = self.d.lookupService(CALLER_ID, SERVICE)
+            ports.append(uri_to_port(res[2]))
+        ports.sort()
+        self.assertEqual(ports, list(range(times)))
