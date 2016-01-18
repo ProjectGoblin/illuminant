08f958338bc4d56d4b5e469ef80de56e91dc9a9d
Oopsno <yufeiliu94@gmail.com>
2016-01-18 21:34:41 +0800

    scripts to launch daemon, dockerd & illuminant

---
+++ bin/daemon.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from illuminant.main import daemon_main
+
+if __name__ == '__main__':
+    daemon_main()
---
+++ bin/illuminant.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from illuminant.main import illuminant_main
+
+if __name__ == '__main__':
+    illuminant_main()
